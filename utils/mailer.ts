f6c4f0cdd8e8e68b6ac0723745d0859d7a068256
amen144 <amenjaballi08@gmail.com>
2025-11-02 16:56:12 +0100

    Add TLS option to mailer configuration

--- utils/mailer.ts
+++ utils/mailer.ts
@@ -6,4 +6,6 @@ export const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER, // your Gmail address
     pass: process.env.EMAIL_PASS, // your Gmail App Password
   },
+    tls: { rejectUnauthorized: false },
+  connectionTimeout: 20000
 });
