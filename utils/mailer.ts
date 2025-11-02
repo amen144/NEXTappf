5afbd23fff81ab20ef60982a866978115be3b27e
amen144 <amenjaballi08@gmail.com>
2025-11-02 19:23:21 +0100

    Fix typo in mailer service configuration

--- utils/mailer.ts
+++ utils/mailer.ts
@@ -1,10 +1,11 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
- sservice: "gmail",// STARTTLS
+  service: "gmail", // or use SMTP for custom domains
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER, // your Gmail address
+    pass: process.env.EMAIL_PASS, // your Gmail App Password
   },
-  connectionTimeout: 15000,
+   connectionTimeout: 15000,
 });
+
